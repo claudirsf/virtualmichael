07a319ca55e44a9315cc33ee6854852feac11fdb
loucoclaudio <claudirsf@gmail.com>
2018-05-11 10:59:50 -0300

    Second Git
    second training michael jackson

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 	//presentation with      music->browser only
+	var track = 0;
+	
 	window.onload = function() {
-	   var open = new SpeechSynthesisUtterance('Michael Jackson, the Audio Experience');
+	   var open = new SpeechSynthesisUtterance('Virtual Michael Jackson Demo , Smooth Criminal');
        open.lang = 'en';
        window.speechSynthesis.speak(open);
        var open2 = new SpeechSynthesisUtterance('game loaded');
        open2.lang = 'en';
        window.speechSynthesis.speak(open2);
+	   
 	};
 	
 	//game basics
@@ -13,25 +16,73 @@
 
 	//keyboard controls
 	function press(key){
-		if(key==32){music()}
-		if(key==49){voice1()}
-		if(key==50){voice2()}
+		if(key==49){music()}
+		if(key==32){sing()}
 	}
 	
+	
 	function music(){
-	   var m = new Audio('wav/base.wav');
+	   var m = new Audio('wav/scriminal/base.wav');
        m.volume = 0.8;
        m.play();
 	}
 	
-	function voice1(){
-	   var v1 = new Audio('wav/hitby.wav');
-	   v1.volume = 0.8;
-       v1.play();
+	function sing(){
+	   
+	switch (track){
+	case 0:
+        var s = new Audio('wav/scriminal/001.wav');
+		s.play();
+		track++;
+        break;
+    case 1:
+        var s = new Audio('wav/scriminal/0001.wav');
+		s.play();
+		track ++;
+        break;
+    case 2:
+        var s = new Audio('wav/scriminal/0002.wav');
+		s.play();
+		track ++;        
+        break;
+    case 3:
+        var s = new Audio('wav/scriminal/0003.wav');
+		s.play();
+		track ++;        
+        break;
+    case 4:
+        var s = new Audio('wav/scriminal/0004.wav');
+		s.play();
+		track ++;        
+        break;
+	case 5:
+        var s = new Audio('wav/scriminal/0005.wav');
+		s.play();
+		track ++;        
+        break;
+	case 6:
+        var s = new Audio('wav/scriminal/0006.wav');
+		s.play();
+		track ++;        
+        break;
+	case 7:
+        var s = new Audio('wav/scriminal/0007.wav');
+		s.play();
+		track ++;        
+        break;
+	case 8:
+        var s = new Audio('wav/scriminal/0008.wav');
+		s.play();
+		track ++;    
+        break;
+	case 9:
+	    var s = new Audio('wav/scriminal/0009.wav');
+		s.play();
+		track ++;
+	case 10:
+	    var s = new Audio('wav/scriminal/0010.wav');
+		s.play();
+		track ++; 
+		
 	}
-	
-	function voice2(){
-	   var v2 = new Audio('wav/smooth.wav');
-       v2.volume = 0.8;
-       v2.play();
 	}
